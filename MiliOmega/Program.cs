b637548ede827aee67c2ab70ab0ff4161c53ae8c
captain-milous <milda.tesar3@gmail.com>
2023-05-24 13:01:24 +0200

    fddghbdfs

--- MiliOmega/Program.cs
+++ MiliOmega/Program.cs
@@ -12,12 +12,17 @@
             CeaserovaSifra ceasar = new CeaserovaSifra(test,"a=c");
 
             Console.WriteLine(ceasar.ToString());
-            string test2 = ceasar.EncryptedText;
+            /*string test2 = ceasar.EncryptedText;
             Console.WriteLine(test2);
 
             ceasar = new CeaserovaSifra(test2, "a=c", true);
             
-            Console.WriteLine(ceasar.ToString());
+            Console.WriteLine(ceasar.ToString());*/
+
+
+            CislaMistoPismen cp = new CislaMistoPismen(test);
+
+            Console.WriteLine(cp.ToString());
         }
     }
 }
